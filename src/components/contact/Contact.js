4195c8fb0efac5486da383a97e958e8728106d97
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2022-12-05 22:36:00 -0500

    added contact page and contact css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,14 @@
 
 const Contact = () => {
   return (
-    <>
-    </>
+    <div className="contact-page">
+      <h1>How to find me?</h1>
+      <img src="https://media-exp1.licdn.com/dms/image/C4E03AQElIPCAuB9_wA/profile-displayphoto-shrink_400_400/0/1654572800559?e=1675900800&v=beta&t=ntUjX7TU9oYKmrWLHLuNmJI43JHcJiZgFFZlljjCY2Y" alt="stephanie sadowski" width={200} className='contact-img' />
+      <br />
+      <button>Github</button>
+      <br />
+      <button>LinkedIn</button>
+    </div>
   )
 }
 
