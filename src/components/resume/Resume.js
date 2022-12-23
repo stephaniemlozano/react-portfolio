26f5b900f5591e2e69d9b09f3029d25f6c91b52d
stephaniemlozano <stephaniesadowski@Stephanies-Air.home>
2022-12-23 14:17:30 -0500

    add resume and css to resume

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,8 +1,10 @@
-
 const Resume = () => {
   return (
-    <>
-    </>
+    <div className="resume">
+      <a href="https://stephanie-buckette.s3.amazonaws.com/resume.png" target="_blank" rel="noopener noreferrer">Click to open my resume in new tab.</a>
+      <br />
+      <img src="https://stephanie-buckette.s3.amazonaws.com/resume.png" alt="resume" width={550} />
+    </div>
   )
 }
 
